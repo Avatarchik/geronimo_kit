48ee52502cc7c71edcad23b96a21133817d2f03d
Sergey Popov <27serg@gmail.com>
2018-08-25 18:51:49 +0600

    Initial commit;

---
+++ Assets/App/Scripts/UI/Buttons/Basic/BasicButtonController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class BasicButtonController : MonoBehaviour
+{
+    public BasicButton[] Buttons;
+
+    private void Start()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            foreach (var button in Buttons)
+            {
+                button.OnClick += Button_OnClick;
+            }
+        }
+    }
+
+    private void Button_OnClick()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            foreach (var button in Buttons)
+            {
+                button.InitialState();
+            }
+        }
+    }
+}
---
+++ Assets/App/Scripts/UI/Panels/MenuPanel.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using UnityEngine;
+
+public class MenuPanel : BasicPanel
+{
+    [Header("Components")]
+    [SerializeField] private BasicButton BtnHome;
+    [SerializeField] private BasicButton BtnProfile;
+    [SerializeField] private BasicButton BtnAboutUs;
+    [SerializeField] private BasicButton BtnPortfolio;
+    [SerializeField] private BasicButton BtnSettings;
+    [SerializeField] private BasicButton BtnLogout;
+
+    public event Action OnHome;
+    public event Action OnProfile;
+    public event Action OnAboutUs;
+    public event Action OnPortfolio;
+    public event Action OnSettings;
+    public event Action OnLogout;
+
+    protected override void Start()
+    {
+        base.Start();
+
+        BtnHome.OnClick += BtnHome_OnClick;
+        BtnProfile.OnClick += BtnProfile_OnClick;
+        BtnAboutUs.OnClick += BtnAboutUs_OnClick;
+        BtnPortfolio.OnClick += BtnPortfolio_OnClick;
+        BtnSettings.OnClick += BtnSettings_OnClick;
+        BtnLogout.OnClick += BtnLogout_OnClick;
+    }
+
+    private void BtnHome_OnClick()
+    {
+        if (OnHome != null)
+        {
+            OnHome();
+        }
+    }
+
+    private void BtnProfile_OnClick()
+    {
+        if (OnProfile != null)
+        {
+            OnProfile();
+        }
+    }
+
+    private void BtnAboutUs_OnClick()
+    {
+        if (OnAboutUs != null)
+        {
+            OnAboutUs();
+        }
+    }
+
+    private void BtnPortfolio_OnClick()
+    {
+        if (OnPortfolio != null)
+        {
+            OnPortfolio();
+        }
+    }
+
+    private void BtnSettings_OnClick()
+    {
+        if (OnSettings != null)
+        {
+            OnSettings();
+        }
+    }
+
+    private void BtnLogout_OnClick()
+    {
+        if (OnLogout != null)
+        {
+            OnLogout();
+        }
+    }
+}
